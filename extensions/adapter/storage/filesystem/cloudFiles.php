cfaa021ad47d0682732e255f0bb2b5fc748afc89
Marius Kubilius <marius.kubilius@gmail.com>
2012-10-10 16:32:20 +0300

    Initial method declaration before writting tests

--- extensions/adapter/storage/filesystem/cloudFiles.php
+++ extensions/adapter/storage/filesystem/cloudFiles.php
@@ -3,6 +3,16 @@ namespace li3_filesystem_cloudfiles\extensions\adapter\storage\filesystem;
 
 class cloudFiles extends \lithium\core\object {
 	
+	
+	/**
+     * @param string $filename
+     * @param string $data
+     * @param array $options
+     * @return mixed returns filename or false otherwise.
+     */
+     public function write($filename, $data, array $options = array()){
+     	
+     }
 }
 
 ?>
